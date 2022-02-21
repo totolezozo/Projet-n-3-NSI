446f2f46ea259e4bd3c8bfba6fab40c0720f81c6
DenizHusson <86835727+DenizHusson@users.noreply.github.com>
2022-02-21 11:39:58 +0100

    Update PNSI3-Arbre.py

--- PNSI3-Arbre.py
+++ PNSI3-Arbre.py
@@ -1,4 +1,5 @@
 
+
 #Class pour créer l'arbre qui servira à faire les suites de questions
 class CréationArbre:
     """Définition de la classe arbre Binaire à partir du site de
@@ -43,10 +44,14 @@ class CréationArbre:
         return f'({self.valeur}, {self.enfant_gauche}, {self.enfant_droit})'
 
 if __name__ == "__main__":
+    
+#Racine de l'arbre, création des 2 premières branches
     racine = CréationArbre(dico[0])
     racine.insert_gauche(dico[1])
     racine.insert_droit(dico[2])
     
+    
+#Premier niveau de noeuds, alpha / beta
     alpha_node = racine.get_gauche()
     alpha_node.insert_gauche(dico[3])
     alpha_node.insert_droit(dico[4])
@@ -55,6 +60,8 @@ if __name__ == "__main__":
     beta_node.insert_gauche(dico[5])
     beta_node.insert_droit(dico[6])
 
+
+#Deuxième niveau de noeuds, gamma ; delta / epsilon ; zeta
     gamma_node = alpha_node.get_gauche()
     gamma_node.insert_gauche(dico[7])
     gamma_node.insert_droit(dico[8])
@@ -70,5 +77,40 @@ if __name__ == "__main__":
     zeta_node = beta_node.get_droit()
     zeta_node.insert_gauche(dico[13])
     zeta_node.insert_droit(dico[14])
-
+    
+    
+#Troisième niveau de noeuds, eta ; theta / iota ; kappa / lambda ; mu /
+    eta_node = gamma_node.get_gauche()
+    eta_node.insert_gauche(dico[15])
+    eta_node.insert_droit(dico[16])
+    
+    theta_node = gamma_node.get_droit()
+    theta_node.insert_gauche(dico[17])
+    theta_node.insert_droit(dico[18])
+    
+    iota_node = delta_node.get_gauche()
+    iota_node.insert_gauche(dico[19])
+    iota_node.insert_droit(dico[20])
+    
+    kappa_node = delta_node.get_droit()
+    kappa_node.insert_gauche(dico[21])
+    kappa_node.insert_droit(dico[22])
+    
+    lambda_node = epsilon_node.get_gauche()
+    lambda_node.insert_gauche(dico[23])
+    lambda_node.insert_droit(dico[24])
+    
+    mu_node = epsilon_node.get_droit()
+    mu_node.insert_gauche(dico[25])
+    mu_node.insert_droit(dico[26])
+    
+    nu_node = zeta_node.get_gauche()
+    nu_node.insert_gauche(dico[27])
+    nu_node.insert_droit(dico[28])
+    
+    xi_node = zeta_node.get_droit()
+    xi_node.insert_gauche(dico[29])
+    xi_node.insert_droit(dico[30])
+    
+    
     print(racine)
