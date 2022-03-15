76272a4a5ca9f0e9827cd3cddb78b19b0bd7bd17
DenizHusson <86835727+DenizHusson@users.noreply.github.com>
2022-03-15 14:47:56 +0100

    Create ImplementationPNSI.py

---
+++ ImplementationPNSI.py
@@ -0,0 +1,46 @@
+
+"""
+
+Première partie des données servant pour l'arbre
+
+dico[1] = {"Désirez-vous vous rendre sur les lieux du crime ? ","la mission continue","le chef de la police vous remplace sur cette mission et vous partez en vacances "}
+dico[2] = {"Une fois sur les lieux du crime, souhaitez vous interroger Thomas l'ainé de la fraterie ?","Thomas vous révèle qu'au moment du crime il était avec Julie dans le salon en train de ranger des livres dans la bibliothèque","Vous pouvez interroger quelqu'un d'autre "}
+dico[3] = {"Désirez vous interroger Julie pour confirmer les dires de Thomas ?","Julie vous confirme les faits","Vous pouvez interroger quelqu'un d'autre "}
+dico[4] = {"Souhaitez vous interroger Julie ?","Julie vous révèle qu'au moment du crime elle était avec Thomas dans le salon en train de ranger des livres dans la bibliothèque","Vous pouvez interroger quelqu'un d'autre"}
+dico[5] = {"Souhaitez vous inerroger Deniz ?","Deniz vous dévoile qu'il était seul dans sa chambre en train de réviser ses partiels","Vous pouvez interroger quelqu'un d'autre"}
+dico[6] = {"Souhaitez vous interroger Deniz ?","Deniz vous dévoile qu'il était seul dans sa chambre en train de réviser ses partiels","Vous pouvez interroger quelqu'un d'autre"}
+dico[7] = {"Souhaitez vous interroger Deniz ?","Deniz vous dévoile qu'il était seul dans sa chambre en train de réviser ses partiels","Vous pouvez interroger quelqu'un d'autre"}
+dico[8] = {"Souhaitez vous interroger Deniz ?","Deniz vous dévoile qu'il était seul dans sa chambre en train de réviser ses partiels","Vous pouvez interroger quelqu'un d'autre"}
+dico[9] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous n'avez plus personne à interroger c'est la fin de l'interrogatoire"}
+dico[10] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous n'avez plus personne à interroger c'est la fin de l'interrogatoire"}
+dico[11] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous n'avez plus personne à interroger c'est la fin de l'interrogatoire"}
+dico[12] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous n'avez plus personne à interroger c'est la fin de l'interrogatoire"}
+dico[13] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous n'avez plus personne à interroger c'est la fin de l'interrogatoire"}
+dico[14] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous n'avez plus personne à interroger c'est la fin de l'interrogatoire"}
+dico[15] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous n'avez plus personne à interroger c'est la fin de l'interrogatoire"}
+dico[16] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous n'avez plus personne à interroger c'est la fin de l'interrogatoire"}
+
+"""
+
+"""
+
+Seconde partie des données servant pour l'arbre
+
+dic[1] = {"Maintenant que vous avez fini d'inerroger tout le monde, souhaitez faire une inspection des chambres pour vérifier les alibis de chacun ?","Vous continuez votre enquête pour avoir plus de preuve","Vous arrêter l'enquête ici et vos éléments ne sont pas suffisants pour accuser quelqu'un, vous êtes donc licencié"}
+dic[2] = {"Souhaitez vous inspecter la chambre de deniz ?","Vous découvrez que les livres et cahiers de deniz sont toujours ouverts et que l'encre est à peine cèche ce qui confirme l'alibi de deniz","Vous pouvez inspecter une autre pièce " }
+dic[3] = {"Souhaitez vous inspecter l'infirmerie où Margaux prépare ses piqures ?","Vous découvrez que l'infirmerie était en ordre pour pouvoir faire la piqure et vous déduisez que de toute façon, Margaux n'ayant pas de part dans l'héritage, elle n'aurait pas de mobile de tuer Hervé","Vous pouvez inspecter une autre pièce"}
+dic[4] = {"Souhaitez vous inspecter l'infirmerie où Margaux prépare ses piqures ?","Vous découvrez que l'infirmerie était en ordre pour pouvoir faire la piqure et vous déduisez que de toute façon, Margaux n'ayant pas de part dans l'héritage, elle n'aurait pas de mobile de tuer Hervé","Vous pouvez inspecter une autre pièce"}
+dic[5] = {"Souhaitez vous vérifier si les livres dans la bibliothèque du salon", "Vous découvrez qu'il n'y a pas de livre dans cette bibliothèque mais des dessins d'Hervé", "Vous pouvez inspecter une autre pièce"}
+dic[6] = {"Souhaitez vous vérifier si les livres dans la bibliothèque du salon", "Vous découvrez qu'il n'y a pas de livre dans cette bibliothèque mais des dessins d'Hervé", "Vous pouvez inspecter une autre pièce"}
+dic[7] = {"Souhaitez vous vérifier si les livres dans la bibliothèque du salon", "Vous découvrez qu'il n'y a pas de livre dans cette bibliothèque mais des dessins d'Hervé", "Vous pouvez inspecter une autre pièce"}
+dic[8] = {"Souhaitez vous vérifier si les livres dans la bibliothèque du salon", "Vous découvrez qu'il n'y a pas de livre dans cette bibliothèque mais des dessins d'Hervé", "Vous pouvez inspecter une autre pièce"} 
+dic[9] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "C'est la fin de l'inspection des pièces, à l'aide des éléments que vous disposez il vous faut désignez un coupable "}
+dic[10] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "C'est la fin de l'inspection des pièces, à l'aide des éléments que vous disposez il vous faut désignez un coupable "}
+dic[11] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "C'est la fin de l'inspection des pièces, à l'aide des éléments que vous disposez il vous faut désignez un coupable "}
+dic[12] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "C'est la fin de l'inspection des pièces, à l'aide des éléments que vous disposez il vous faut désignez un coupable "}
+dic[13] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "C'est la fin de l'inspection des pièces, à l'aide des éléments que vous disposez il vous faut désignez un coupable "}
+dic[14] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "C'est la fin de l'inspection des pièces, à l'aide des éléments que vous disposez il vous faut désignez un coupable "}
+dic[15] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "C'est la fin de l'inspection des pièces, à l'aide des éléments que vous disposez il vous faut désignez un coupable "}
+dic[16] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "C'est la fin de l'inspection des pièces, à l'aide des éléments que vous disposez il vous faut désignez un coupable "}
+
+"""
