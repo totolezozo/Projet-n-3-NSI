ac7243ad245e4c4d300b7f6739335339109fb376
Ulysse92dz <96249808+Ulysse92dz@users.noreply.github.com>
2022-03-20 13:36:40 +0100

    Update ImplementationPNSI.py

--- ImplementationPNSI.py
+++ ImplementationPNSI.py
@@ -1,3 +1,4 @@
+#C'est bon j'ai fini, finalement c'est qu'un seul arbre et quand ya un espace entre deux blocs de lignes c'est genre qu'on passe à l'étage d'en dessous dans l'arbre
 dico[1] = {"Désirez-vous vous rendre sur les lieux du crime ? ","la mission continue","le chef de la police vous remplace sur cette mission et vous partez en vacances "} 
 
 dico[2] = {"Une fois sur les lieux du crime, souhaitez vous interroger Thomas l'ainé de la fraterie ?","Thomas vous révèle qu'au moment du crime il était avec Julie dans le salon en train de ranger des livres dans la bibliothèque","Vous pouvez interroger quelqu'un d'autre "}
@@ -36,5 +37,49 @@ dico[30] = {"Souhaitez vous interroger Julie ?","Julie vous révèle qu'au momen
 dico[31] = {"Souhaitez vous vérifier si les livres dans la bibliothèque du salon sont bien rangés ?", "Vous découvrez qu'il n'y a pas de livre dans cette bibliothèque mais des dessins d'Hervé", "Vous pouvez faire autre chose"}
 dico[32] = {"C'est la fin de l'enquête"}
 
+dico[33] = {"Souhaitez vous inspecter la chambre de deniz ?","Vous découvrez que les livres et cahiers de deniz sont toujours ouverts et que l'encre est à peine sèche ce qui confirme l'alibi de deniz","Vous pouvez faire autre chose" }
+dico[34] = {"C'est la fin de l'enquête"}
+dico[35] = {"Souhaitez vous inspecter l'infirmerie où Margaux prépare ses piqures ?","Vous découvrez que l'infirmerie était en ordre pour pouvoir faire la piqure et vous déduisez que de toute façon, Margaux n'ayant pas de part dans l'héritage, elle n'aurait pas de mobile de tuer Hervé","Vous pouvez faire autre chose"}
+dico[36] = {"C'est la fin de l'enquête"}
+dico[37] = {"Souhaitez vous inspecter l'infirmerie où Margaux prépare ses piqures ?","Vous découvrez que l'infirmerie était en ordre pour pouvoir faire la piqure et vous déduisez que de toute façon, Margaux n'ayant pas de part dans l'héritage, elle n'aurait pas de mobile de tuer Hervé","Vous pouvez faire autre chose"}
+dico[38] = {"C'est la fin de l'enquête"}
+dico[39] = {"Souhaitez vous interroger Julie ?","Julie vous révèle qu'au moment du crime elle était avec Thomas dans le salon en train de ranger des livres dans la bibliothèque","Vous pouvez faire autre chose"}
+dico[40] = {"Souhaitez vous interroger Julie ?","Julie vous révèle qu'au moment du crime elle était avec Thomas dans le salon en train de ranger des livres dans la bibliothèque","Vous pouvez faire autre chose"}
+dico[41] = {"Souhaitez vous vérifier si les livres dans la bibliothèque du salon sont bien rangés ?", "Vous découvrez qu'il n'y a pas de livre dans cette bibliothèque mais des dessins d'Hervé", "Vous pouvez faire autre chose"}
+dico[42] = {"C'est la fin de l'enquête"}
+dico[43] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "Vous pouvez faire autre chose"}
+dico[44] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous pouvez faire autre chose"}
+dico[45] = {"Souhaitez vous inspecter l'infirmerie où Margaux prépare ses piqures ?","Vous découvrez que l'infirmerie était en ordre pour pouvoir faire la piqure et vous déduisez que de toute façon, Margaux n'ayant pas de part dans l'héritage, elle n'aurait pas de mobile de tuer Hervé","Vous pouvez faire autre chose"}
+dico[46] = {"C'est la fin de l'enquête"}
+dico[47] = {"Souhaitez vous vérifier si les livres dans la bibliothèque du salon sont bien rangés ?", "Vous découvrez qu'il n'y a pas de livre dans cette bibliothèque mais des dessins d'Hervé", "Vous pouvez faire autre chose"}
+dico[48] = {"C'est la fin de l'enquête"}
+dico[49] = {"Souhaitez vous vérifier si les livres dans la bibliothèque du salon sont bien rangés ?", "Vous découvrez qu'il n'y a pas de livre dans cette bibliothèque mais des dessins d'Hervé", "Vous pouvez faire autre chose"}
+dico[50] = {"C'est la fin de l'enquête"}
+dico[51] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "Vous pouvez faire autre chose"}
+dico[52] = {"C'est la fin de l'enquête"}
 
- #J'ai pas fini ça arrive bientôt la suite
+dico[53] = {"Souhaitez vous vérifier si les livres dans la bibliothèque du salon sont bien rangés ?", "Vous découvrez qu'il n'y a pas de livre dans cette bibliothèque mais des dessins d'Hervé", "Vous pouvez faire autre chose"}
+dico[54] = {"C'est la fin de l'enquête"}
+dico[55] = {"Souhaitez vous vérifier si les livres dans la bibliothèque du salon sont bien rangés ?", "Vous découvrez qu'il n'y a pas de livre dans cette bibliothèque mais des dessins d'Hervé", "Vous pouvez faire autre chose"}
+dico[56] = {"C'est la fin de l'enquête"}
+dico[57] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "Vous pouvez faire autre chose"}
+dico[58] = {"C'est la fin de l'enquête"}
+dico[59] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous pouvez faire autre chose"}
+dico[60] = {"Souhaitez vous interroger Margaux ? ","Margaux vous explique qu'après avoir préparer la piqure habituelle d'Hervé, elle découvre son corps inanimé dans la baignoire avec un liquide violet mélangé à l'eau","Vous pouvez faire autre chose"}
+dico[61] = {"Souhaitez vous inspecter l'infirmerie où Margaux prépare ses piqures ?","Vous découvrez que l'infirmerie était en ordre pour pouvoir faire la piqure et vous déduisez que de toute façon, Margaux n'ayant pas de part dans l'héritage, elle n'aurait pas de mobile de tuer Hervé","Vous pouvez faire autre chose"}
+dico[62] = {"C'est la fin de l'enquête"}
+dico[63] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "Vous pouvez faire autre chose"}
+dico[64] = {"C'est la fin de l'enquête"}
+dico[65] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "Vous pouvez faire autre chose"}
+dico[66] = {"C'est la fin de l'enquête"}
+
+dico[67] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "Vous pouvez faire autre chose"}
+dico[68] = {"C'est la fin de l'enquête"}
+dico[69] = {"Souhaitez vous inspecter la chambre de Julie ?", "Vous découvrez que dans le fond d'un tirroir une substance dont le labo vous confirme sa dangerosité si elle est diluée dans l'eau", "Vous pouvez faire autre chose"}
+dico[70] = {"C'est la fin de l'enquête"}
+dico[71] = {"Souhaitez vous inspecter l'infirmerie où Margaux prépare ses piqures ?","Vous découvrez que l'infirmerie était en ordre pour pouvoir faire la piqure et vous déduisez que de toute façon, Margaux n'ayant pas de part dans l'héritage, elle n'aurait pas de mobile de tuer Hervé","Vous pouvez faire autre chose"}
+dico[72] = {"C'est la fin de l'enquête"}
+dico[73] = {"Souhaitez vous inspecter l'infirmerie où Margaux prépare ses piqures ?","Vous découvrez que l'infirmerie était en ordre pour pouvoir faire la piqure et vous déduisez que de toute façon, Margaux n'ayant pas de part dans l'héritage, elle n'aurait pas de mobile de tuer Hervé","Vous pouvez faire autre chose"}
+dico[74] = {"C'est la fin de l'enquête"}
+
+
